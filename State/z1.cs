e901460b0ba859315b0314e6c253560dfec101d1
owr <proroog@gmail.com>
2025-11-27 18:52:17 +0100

    State

---
+++ State/z1.cs
@@ -0,0 +1,216 @@
+﻿namespace State
+{
+
+    public class StateContext
+    {
+        private State _currentState;
+        public int Balance { get; set; }
+
+        public StateContext(int balance)
+        {
+            Balance = balance;
+
+            if(Balance > 0)
+            {
+                _currentState = new NoCardState(this);
+            }
+            else
+            {
+                _currentState = new NoCashState(this);
+            }
+        }
+
+        public void ChangeState(State state)
+        {
+            _currentState = state;
+        }
+
+        public void InsertCard()
+        {
+            _currentState.InsertCard();
+        }
+
+        public void EjectCard()
+        {
+            _currentState.EjectCard();
+        }
+
+        public void InsertPin(int pin)
+        {
+            _currentState.InsertPin(pin);
+        }
+
+        public void WithdrawCash(int amount)
+        {
+            _currentState.WithdrawCash(amount);
+        }
+    }
+
+    public abstract class State
+    {
+        protected StateContext _context;
+
+        protected State(StateContext context)
+        {
+            _context = context;
+        }
+
+        public abstract void InsertCard();
+        public abstract void EjectCard();
+        public abstract void InsertPin(int pin);
+        public abstract void WithdrawCash(int amount);
+    }
+
+    public class NoCardState : State
+    {
+        public NoCardState(StateContext context) : base(context) { }
+
+        public override void InsertCard()
+        {
+            Console.WriteLine("Włożono kartę.");
+            _context.ChangeState(new CardInsertedState(_context));
+        }
+
+        public override void EjectCard()
+        {
+            Console.WriteLine("Brak karty do wyjęcia.");
+        }
+
+        public override void InsertPin(int pin)
+        {
+            Console.WriteLine("Brak karty!");
+        }
+
+        public override void WithdrawCash(int amount)
+        {
+            Console.WriteLine("Nie można wypłacić pieniędzy! Najpierw włóż kartę!");
+        }
+    }
+
+    public class CardInsertedState : State
+    {
+        public CardInsertedState(StateContext context) : base(context) { }
+
+        public override void InsertCard()
+        {
+            Console.WriteLine("Karta została już włożona!");
+        }
+
+        public override void EjectCard()
+        {
+            Console.WriteLine("Karta została wyjęta.");
+            _context.ChangeState(new NoCardState(_context));
+        }
+
+        public override void InsertPin(int pin)
+        {
+            if(pin == 1234)
+            {
+                Console.WriteLine("PIN poprawny.");
+                _context.ChangeState(new PinInsertedState(_context));
+            }
+            else
+            {
+                Console.WriteLine("Błędny kod PIN!");
+            }
+        }
+
+        public override void WithdrawCash(int amount)
+        {
+            Console.WriteLine("Najpierw wprowadź PIN!");
+        }
+    }
+
+    public class PinInsertedState : State
+    {
+        public PinInsertedState(StateContext context) : base(context) { }
+
+        public override void InsertCard()
+        {
+            Console.WriteLine("Karta została już włożona!");
+        }
+
+        public override void EjectCard()
+        {
+            Console.WriteLine("Karta została wyjęta.");
+            _context.ChangeState(new NoCardState(_context));
+        }
+
+        public override void InsertPin(int pin)
+        {
+            Console.WriteLine("PIN został już wprowadzony!");
+        }
+
+        public override void WithdrawCash(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("Kwota musi być większa niz 0!");
+                return;
+            }
+
+            if(amount > _context.Balance)
+            {
+                Console.WriteLine("Bankomat nie posiada tyle gotówki!");
+                return;
+            }
+
+            _context.Balance -= amount;
+            Console.WriteLine($"Wypłacono {amount}PLN. Pozostało {_context.Balance}PLN.\nDziękujemy za skorzystanie z usług bankomatu.");
+            
+            if(_context.Balance == 0)
+            {
+                _context.ChangeState(new NoCashState(_context));
+            }
+            else
+            {
+                _context.ChangeState(new NoCardState(_context));
+            }
+        }
+    }
+
+    public class NoCashState : State
+    {
+        public NoCashState(StateContext context) : base(context) { }
+
+        public override void InsertCard()
+        {
+            Console.WriteLine("Bankomat nie ma gotówki. Operacja niedostepna.");
+        }
+
+        public override void EjectCard()
+        {
+            Console.WriteLine("Brak karty do wyjęcia.");
+        }
+
+        public override void InsertPin(int pin)
+        {
+            Console.WriteLine("Banomat nie ma gotówki.");
+        }
+
+        public override void WithdrawCash(int amount)
+        {
+            Console.WriteLine("Brak dostępnych środków w bankomacie.");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StateContext atmContext = new(500);
+
+            atmContext.InsertCard();
+            atmContext.InsertPin(1234);
+            atmContext.WithdrawCash(200);
+
+            atmContext.InsertCard();
+            atmContext.InsertPin(4321);
+            atmContext.InsertPin(1234);
+            atmContext.WithdrawCash(400);
+            atmContext.WithdrawCash(300);
+
+            atmContext.InsertCard();
+        }
+    }
+}
---
+++ State/z2.cs
@@ -0,0 +1,233 @@
+﻿namespace State
+{
+    public interface IVendingMachineState
+    {
+        void InsertMoney(int amount);
+        void SelectProduct(string productName);
+        void WithdrawMoney();
+    }
+
+    public class VendingMachine
+    {
+        private IVendingMachineState _currentState { get; set; }
+        public IVendingMachineState CurrentState { get => _currentState; set => _currentState = value; }
+
+        public Dictionary<string, (int Price, int Quantity)> Products { get; private set; }
+        public int CurrentBalance { get; set; }
+        public string SelectedProduct { get; set; }
+
+        public VendingMachine()
+        {
+            Products = new() {
+                { "Cola", (5, 5) },
+                { "Woda", (2, 2) },
+                { "Sok", (4, 6) },
+                { "Nestea", (5, 0) }
+            };
+
+            _currentState = new WaitingForMoneyState(this);
+            CurrentBalance = 0;
+            SelectedProduct = null;
+        }
+
+        public void InsertMoney(int amount)
+        {
+            _currentState.InsertMoney(amount);
+        }
+
+        public void SelectProduct(string productName)
+        {
+            _currentState.SelectProduct(productName);
+        }
+
+        public void WithdrawMoney()
+        {
+            if(CurrentBalance <= 0)
+            {
+                Console.WriteLine("Brak środków do wpyłaty!");
+                return;
+            }
+
+            Console.WriteLine($"Wydaje {CurrentBalance}PLN...");
+            CurrentBalance = 0;
+            _currentState = new WaitingForMoneyState(this);
+        }
+
+        public void DispenseProduct()
+        {
+            if(SelectedProduct == null)
+            {
+                Console.WriteLine("Wybierz produkt!");
+                return;
+            }
+
+            if (!Products.ContainsKey(SelectedProduct))
+            {
+                Console.WriteLine("Nie ma takiego produktu!");
+                return;
+            }
+
+            var product = Products[SelectedProduct];
+
+            if(product.Quantity == 0)
+            {
+                Console.WriteLine($"Brak na stanie produktu {SelectedProduct}");
+                _currentState = new WaitingForMoneyState(this);
+                return;
+            }
+
+            if(product.Price > CurrentBalance)
+            {
+                Console.WriteLine($"Niewystarczająca ilość pieniędzy! Ten produkt kosztuje {product.Price}PLN.");
+                _currentState = new WaitingForMoneyState(this);
+                return;
+            }
+
+            Products[SelectedProduct] = (product.Price, product.Quantity - 1);
+            CurrentBalance -= product.Price;
+            Console.WriteLine($"Wydano {SelectedProduct}. Reszta: {CurrentBalance}PLN.");
+            CurrentBalance = 0;
+            SelectedProduct = null;
+            _currentState = new WaitingForMoneyState(this);
+        }
+    }
+
+    public class WaitingForMoneyState : IVendingMachineState
+    {
+        private readonly VendingMachine _vMachine;
+
+        public WaitingForMoneyState(VendingMachine vMachine)
+        {
+            _vMachine = vMachine;
+        }
+
+        public void InsertMoney(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("Kwota musi być dodatnia!");
+                return;
+            }
+
+            _vMachine.CurrentBalance += amount;
+            Console.WriteLine($"Pomyślnie włożono {amount}PLN. Aktualny stan: {_vMachine.CurrentBalance}PLN.");
+            _vMachine.CurrentState = new ProductSelectionState(_vMachine);
+        }
+
+        public void SelectProduct(string productName)
+        {
+            Console.WriteLine("Najpierw włóż pieniądze!");
+        }
+
+        public void WithdrawMoney()
+        {
+            if (_vMachine.CurrentBalance <= 0)
+            {
+                Console.WriteLine("Brak środków do wpyłaty!");
+                return;
+            }
+
+            Console.WriteLine($"Wydaje {_vMachine.CurrentBalance}PLN...");
+            _vMachine.CurrentBalance = 0;
+            _vMachine.CurrentState = new WaitingForMoneyState(_vMachine);
+        }
+    }
+
+    public class ProductSelectionState : IVendingMachineState
+    {
+        private readonly VendingMachine _vMachine;
+
+        public ProductSelectionState(VendingMachine vMachine)
+        {
+            _vMachine = vMachine;
+        }
+
+        public void InsertMoney(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Kwota musi być dodatnia!");
+                return;
+            }
+
+            _vMachine.CurrentBalance += amount;
+            Console.WriteLine($"Pomyślnie włożono {amount}PLN. Aktualny stan: {_vMachine.CurrentBalance}PLN.");
+            _vMachine.CurrentState = new ProductSelectionState(_vMachine);
+        }
+
+        public void SelectProduct(string productName)
+        {
+            if (!_vMachine.Products.ContainsKey(productName)){
+                Console.WriteLine("Nie ma takiego produktu!");
+                return;
+            }
+
+            _vMachine.SelectedProduct = productName;
+            _vMachine.CurrentState = new FinalizeTransactionState(_vMachine);
+            _vMachine.CurrentState.SelectProduct(productName);
+        }
+        public void WithdrawMoney()
+        {
+            if (_vMachine.CurrentBalance <= 0)
+            {
+                Console.WriteLine("Brak środków do wpyłaty!");
+                return;
+            }
+
+            Console.WriteLine($"Wydaje {_vMachine.CurrentBalance}PLN...");
+            _vMachine.CurrentBalance = 0;
+            _vMachine.CurrentState = new WaitingForMoneyState(_vMachine);
+        }
+    }
+
+    public class FinalizeTransactionState : IVendingMachineState
+    {
+        private readonly VendingMachine _vMachine;
+
+        public FinalizeTransactionState(VendingMachine vMachine)
+        {
+            _vMachine = vMachine;
+        }
+
+        public void InsertMoney(int amount)
+        {
+            Console.WriteLine("Transakcja w toku. Nie można dodawać pieniędzy!");
+        }
+
+        public void SelectProduct(string productName)
+        {
+            _vMachine.DispenseProduct();
+        }
+        public void WithdrawMoney()
+        {
+            Console.WriteLine("Transakcja w toku. Nie można wypłacić środków!");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vendingMachine = new();
+
+            vendingMachine.InsertMoney(2);
+            vendingMachine.InsertMoney(5);
+            vendingMachine.SelectProduct("Cola");
+            vendingMachine.InsertMoney(2);
+            vendingMachine.SelectProduct("Woda");
+            vendingMachine.SelectProduct("Sok");
+            vendingMachine.InsertMoney(1);
+            vendingMachine.SelectProduct("Sok");
+            vendingMachine.InsertMoney(3);
+            vendingMachine.SelectProduct("Sok");
+            vendingMachine.SelectProduct("Nestea");
+            vendingMachine.InsertMoney(2);
+            vendingMachine.SelectProduct("Nestea");
+            vendingMachine.InsertMoney(3);
+            vendingMachine.WithdrawMoney();
+            vendingMachine.SelectProduct("Nestea");
+            vendingMachine.InsertMoney(5);
+            vendingMachine.SelectProduct("Nestea");
+        }
+    }
+}
