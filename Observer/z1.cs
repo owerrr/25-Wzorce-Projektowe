0c6edac4b299a574c5a91ff91ffc689cee0ed7da
owr <proroog@gmail.com>
2025-11-28 12:33:43 +0100

    Observer

---
+++ Observer/z1.cs
@@ -0,0 +1,120 @@
+﻿namespace Observer
+{
+    public interface IObserver
+    {
+        public void Update(float temp, float humi, float press);
+    }
+    public interface ISubject
+    {
+        public void RegisterObserver(IObserver obs);
+        public void UnregisterObserver(IObserver obs);
+        public void NotifyObservers();
+    }
+
+    public class WeatherStation : ISubject
+    {
+        private List<IObserver> _observers = new();
+        private float _temp;
+        private float _humi;
+        private float _press;
+
+        public void NotifyObservers()
+        {
+            foreach (var obs in _observers)
+            {
+                obs.Update(_temp, _humi, _press);
+            }
+        }
+
+        public void RegisterObserver(IObserver obs)
+        {
+            _observers.Add(obs);
+            Console.WriteLine("Dodano nowy observer!");
+        }
+
+        public void UnregisterObserver(IObserver obs)
+        {
+            _observers.Remove(obs);
+            Console.WriteLine("Usunięto observer!");
+        }
+
+        public void UpdateMeasurements(float temp, float humi, float press)
+        {
+            _temp = temp;
+            _humi = humi;
+            _press = press;
+            NotifyObservers();
+        }
+    }
+
+    public class MobileDisplay : IObserver
+    {
+        private WeatherStation _weatherStation;
+        private float _temp;
+        private float _humi;
+        private float _press;
+
+        public MobileDisplay(WeatherStation weatherStation)
+        {
+            _weatherStation = weatherStation;
+            _weatherStation.RegisterObserver(this);
+        }
+
+        public void Update(float temp, float humi, float press)
+        {
+            _temp = temp;
+            _humi = humi;
+            _press = press;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"MOBILE\nTemperatura: {_temp}\nWilgotność: {_humi}\nCiśnienie: {_press}\n");
+        }
+    }
+
+    public class DesktopDisplay : IObserver
+    {
+        private WeatherStation _weatherStation;
+        private float _temp;
+        private float _humi;
+        private float _press;
+
+        public DesktopDisplay(WeatherStation weatherStation)
+        {
+            _weatherStation = weatherStation;
+            _weatherStation.RegisterObserver(this);
+        }
+
+        public void Update(float temp, float humi, float press)
+        {
+            _temp = temp;
+            _humi = humi;
+            _press = press;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"DESKTOP\nTemperatura: {_temp}\nWilgotność: {_humi}\nCiśnienie: {_press}\n");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherStation weatherStation = new();
+
+            MobileDisplay mobileDisplay = new(weatherStation);
+            DesktopDisplay desktopDisplay = new(weatherStation);
+
+            weatherStation.UpdateMeasurements(15f, 1f, 25f);
+
+            weatherStation.UnregisterObserver(mobileDisplay);
+
+            weatherStation.UpdateMeasurements(10f, 5f, 10f);
+        }
+    }
+}
---
+++ Observer/z2.cs
@@ -0,0 +1,121 @@
+﻿namespace Observer
+{
+    public interface ICurrencyObserver
+    {
+        public void Update(string currency, double value);
+    }
+
+    public interface ISubject
+    {
+        public void Attach(ICurrencyObserver obs);
+        public void Detach(ICurrencyObserver observer);
+        public void Notify();
+    }
+
+    public class CurrencyExchange : ISubject
+    {
+        private List<ICurrencyObserver> _observers = new();
+        private Dictionary<string, double> _currencies = new();
+
+        private string _currency;
+        private double _value;
+     
+        public CurrencyExchange()
+        {
+            _currencies = new()
+            {
+                { "PLN", 1 },
+                { "EUR", 0.24d },
+                { "USD", 0.27d },
+                { "GBP", 0.21d }
+            };
+        }
+
+        public void Attach(ICurrencyObserver obs)
+        {
+            _observers.Add(obs);
+            Console.WriteLine("Dodano nowy observer!");
+        }
+
+        public void Detach(ICurrencyObserver obs)
+        {
+            _observers.Remove(obs);
+            Console.WriteLine("Pomyślnie usunięto observer!");
+        }
+
+        public void Notify()
+        {
+            foreach(var obs in _observers)
+            {
+                obs.Update(_currency, _value);
+            }
+        }
+
+        public void UpdateRate(string currency, double rate)
+        {
+            if (!_currencies.ContainsKey(currency))
+            {
+                Console.WriteLine("\nNieprawidłowa waluta!");
+                return;
+            }
+
+            double currencyValue = _currencies[currency];
+            
+            if(currencyValue + rate <= 0)
+            {
+                Console.WriteLine("\nwartość waluty musi być większa od 0!");
+                return;
+            }
+
+            _currency = currency;
+            _value = currencyValue + rate;
+
+            _currencies[currency] = _value;
+            
+            Console.WriteLine($"\nPomyślnie zaktualizowano walute {currency}. Teraz jest warta: {_currencies[currency]} za 1PLN.");
+            Notify();
+        }
+    }
+
+    public class MobileAppNotifier : ICurrencyObserver
+    {
+        private string _currency;
+        private double _value;
+
+        public void Update(string currency, double value)
+        {
+            _currency = currency;
+            _value = value;
+            Console.WriteLine($"\nMOBILE\nUWAGA! PRZEWALUTOWANIE!\n{_currency} jest teraz warte {_value}{_currency} za 1PLN!");
+        }
+    }
+
+    public class DesktopAppNotifier : ICurrencyObserver
+    {
+        private string _currency;
+        private double _value;
+
+        public void Update(string currency, double value)
+        {
+            _currency = currency;
+            _value = value;
+            Console.WriteLine($"\nDESKTOP\nUWAGA! PRZEWALUTOWANIE!\n{_currency} jest teraz warte {_value}{_currency} za 1PLN!");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CurrencyExchange currencyExchange = new();
+
+            currencyExchange.Attach(new MobileAppNotifier());
+            currencyExchange.Attach(new DesktopAppNotifier());
+
+            currencyExchange.UpdateRate("GBP", 0.1d);
+            currencyExchange.UpdateRate("USD", -0.1d);
+            currencyExchange.UpdateRate("EUR", -5d);
+
+        }
+    }
+}
